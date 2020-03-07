6609301f312c98a41f5947f3dcbbe3bb4537fddb
Mikhail Korovai <korovai.personal@gmail.com>
2020-03-07 19:36:43 +0200

    Updates

---
+++ precache-manifest.27660da60aacd53d0c6dc0fc32c8e3d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b968882e57ae2bdc752a6b9e66d44b2",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "f058f87db984a3064d8f",
+    "url": "/aviasales/static/css/main.9e21324c.chunk.css"
+  },
+  {
+    "revision": "171a05725014f27e54d3",
+    "url": "/aviasales/static/js/2.8d227d97.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/aviasales/static/js/2.8d227d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f058f87db984a3064d8f",
+    "url": "/aviasales/static/js/main.d7f8eb3f.chunk.js"
+  },
+  {
+    "revision": "b87cb3452a8f921d6c69",
+    "url": "/aviasales/static/js/runtime-main.fc40cbca.js"
+  },
+  {
+    "revision": "0cc286b29ade1d9f2dbc2411e4459182",
+    "url": "/aviasales/static/media/logo.0cc286b2.png"
+  }
+]);
